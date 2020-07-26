35fa4ab0602507ac84fc8f297fb680cd657cdce7
AggieWB <usmaggie23@gmail.com>
2020-07-26 14:59:47 -0700

    Change var to const, and remove uneccessary commenting

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 const INVALID_DATE_STRING = 'Invalid Date';
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -15,7 +11,7 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // listen for requests
-var listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
